4a633fe89f8584a9fa76c0ceb6e96145eda98c4d
Fernando Isaac Gonzalez Medina <isaacgonzalez96@aragon.unam.mx>
2024-03-13 17:58:59 -0600

    Falta actualización en escala de fotos

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '33b438ef5f6262bcf7ab');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '72792d54ca7e9b0916f9');
