a4c00fae6c0c327e84c8d64943e9eebcba47c0d7
Fernando Isaac Gonzalez Medina <isaacgonzalez96@aragon.unam.mx>
2024-03-15 17:53:15 -0600

    Faltan modificaciones a escalas

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '72792d54ca7e9b0916f9');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '0882e5ab48a09a329e6d');
